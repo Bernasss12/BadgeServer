c6ab8dbd35fb462ffcc334260b75d8a21cfa74a3
Bernardo Antunes <bernasss12@gmail.com>
2023-07-10 22:46:37 +0100

    Implement new SvgGenerator

--- src/main/kotlin/dev/bernasss12/plugins/Routing.kt
+++ src/main/kotlin/dev/bernasss12/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package dev.bernasss12.plugins
 
+import dev.bernasss12.fetching.CurseforgeDataFetcher
 import dev.bernasss12.fetching.ModrinthDataFetcher
 import dev.bernasss12.templater.Template
 import dev.bernasss12.templater.Templater
@@ -14,31 +15,29 @@ import java.io.File
 fun Application.configureRouting() {
 
     routing {
-        get("/badges/{badge-source}/{mod-id-slug}/{info-type}") {
-            val text: String = when (call.parameters["info-type"]) {
-                "downloads" -> DataFormatter.formatNumberWithSeparators(ModrinthDataFetcher.getDownloadCount(call.parameters["mod-id-slug"]!!), ",")
-                "down" -> DataFormatter.formatNumberShorten(ModrinthDataFetcher.getDownloadCount(call.parameters["mod-id-slug"]!!), false)
-                "name" -> ModrinthDataFetcher.getModName(call.parameters["mod-id-slug"]!!)
-                else -> throw Error("Not valid type.")
-            }
-
-            val xml = when (call.parameters["badge-source"]) {
-                "curseforge" -> curseforge(text)
-                "modrinth" -> modrinth(text)
-                else -> throw Error("Not valid source")
-            }
-
-            call.respondText(xml, ContentType.Image.SVG, HttpStatusCode.OK)
-        }
         get(Regex("/modrinth/(?<modid>[\\w!@\$()`.+,\"\\-']{3,64})(/downloads(/(?<lang>[a-z]{2}))?)?")) {
-            val text = DataFormatter.formatNumberShorten(ModrinthDataFetcher.getDownloadCount(call.parameters["modid"]!!), false)
-            val xml = modrinth(text)
-            call.respondText(xml, ContentType.Image.SVG, HttpStatusCode.OK)
+            call.respondText(
+                text = SvgGenerator.generate(
+                    Template.MODRINTH_ICON_TEXT,
+                    call.parameters["modid"]!!,
+                    ModrinthDataFetcher,
+                    SvgGenerator.DataTypes.DOWNLOADS.toString()
+                ),
+                contentType = ContentType.Image.SVG,
+                status = HttpStatusCode.OK
+            )
         }
         get(Regex("/curseforge/(?<modid>[\\w!@\$()`.+,\"\\-']{3,64})(/downloads(/(?<lang>[a-z]{2}))?)?")) {
-            val text = DataFormatter.formatNumberShorten(ModrinthDataFetcher.getDownloadCount(call.parameters["modid"]!!), false)
-            val xml = curseforge(text)
-            call.respondText(xml, ContentType.Image.SVG, HttpStatusCode.OK)
+            call.respondText(
+                text = SvgGenerator.generate(
+                    Template.CURSEFORGE_ICON_TEXT,
+                    call.parameters["modid"]!!,
+                    CurseforgeDataFetcher,
+                    SvgGenerator.DataTypes.DOWNLOADS.toString()
+                ),
+                contentType = ContentType.Image.SVG,
+                status = HttpStatusCode.OK
+            )
         }
         get("/modrinth/{mod-id-slug}/name") {
             call.respondText(
